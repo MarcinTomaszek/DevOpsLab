1702804c98e84f715c2f56a15a65c6a6ecaf1036
MarcinTomaszek <146821449+MarcinTomaszek@users.noreply.github.com>
2025-11-21 15:21:52 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/api/hello')
-def hello():
-    return jsonify(message="Witaj z backendu Flask! 🚀")
+@app.route("/api/rates")
+def get_rates():
+    url = "https://api.exchangerate.host/latest?base=USD&symbols=EUR,GBP,CHF,PLN"
+    response = requests.get(url).json()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    return jsonify({
+        "base": response["base"],
+        "rates": response["rates"],
+        "date": response["date"]
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
